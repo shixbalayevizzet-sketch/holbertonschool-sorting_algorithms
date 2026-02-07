00064ab9ec0307d01482b5657d8304bbdb7f7928
Izzat <shixbalayevizzet@example.com>
2026-02-07 11:11:06 +0000

    task0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,43 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "sort.h"
 
 /**
- * bubble_sort - Massivi Bubble sort alqoritmi ilə sıralayır.
- * @array: Sıralanacaq massiv (pointer)
- * @size: Massivin ölçüsü
+ * bubble_sort - Sorts an array of integers in ascending order
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
-    int swapped;
+	size_t i, j, k;
+	int temp;
+	int swapped;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        swapped = 0;
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                /* Elementlərin yerini dəyişirik */
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 
-                /* Hər swap-dan sonra massivi çap edirik */
-                for (size_t k = 0; k < size; k++)
-                {
-                    printf("%d%s", array[k], (k < size - 1) ? ", " : "");
-                }
-                printf("\n"); 
-                swapped = 1;
-            }
-        }
-        /* Əgər heç bir swap baş vermədisə, massiv artıq sıralanıb */
-        if (swapped == 0)
-            break;
-    }
+				print_array(array, size);
+				swapped = 1;
+			}
+		}
+		if (swapped == 0)
+			break;
+	}
 }
